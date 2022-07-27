fad7367da27f45d61a68336bbc6d5c600ece066b
garyd <github@gazza.id.au>
2022-07-27 22:17:11 +1000

    sketch basic code outline

---
+++ motivating_example.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel
+
+
+class Hero(BaseModel):
+    """This docstring becomes the documentation in GQL Schema."""
+
+    # TODO do we want a compulsory `id` field? Or jsut use hashing (implementers need to make your hashing correct!)
+
+    # A field known at time of instnaitating this object, possibly with straightforward transofrmation fromthe source field
+    # TODO calrify whether this *must* be a scalar, or can be anything. no reason it can't be anything
+    #: Shebang-based docstring becomes the documentation in GQL Schema
+    name: str  # TODO non-empty; other config
+
+    # TODO need an example of a hidden (non-response) field. What doe spydantic offer us
+
+    # TODO do we need a decorator here?
+    # TODO do we want dependency injection here (arbitrary dependencies, not the crappy graphql-core ones)
+    async def friends(self):
+        # A field not known at instantiation time, that has non-trivial implementaiton (eg. dataabse access, compelxx data combination) and/or accepts GQL parameters
+
+        # Note that return type is non-compulsory, and is not the same as the response type
+
+        # Note that this is a normal pydanitc model, so `self` genuinely is an instance of this class. MOreover, it corresponds to *the*
+        # response object, not  acopy or source for the repsonse object. (response object is transforemed into JSON to go over the wire, but otheriwsie is this object)
+
+        # Object hierarchy is an inherent part of grapohql object reosluiton, so we don't attempt to decompose field resolvers into standalonae functions separated frm a class
+        # Resolvers are reused by having standalone internal funcitons, or by using the same class at different points in the hierarchy
+
+        return []
+
+    # TODO example of using pydantic Config class
+
+
+class Query(BaseModel):
+    pass  # FIXME `hero` with diff parametrs.
+    # TODO example deprecation
+
+
+class Mutation(BaseModel):
+    # todo demonstrate good-practice pattern (input, not splatting at top-level)
+    pass  # FIXME
+
+
+schema = make_schema(
+    query=Query,
+)
---
+++ query_handler_poc.py
@@ -0,0 +1,96 @@
+# fast API handler
+import inspect
+from typing import Any
+
+from pydantic import BaseModel
+
+
+async def handle_graphql_post():
+    # Example for a query operation
+    query_string = get_graphql_operation_content_from_http_request()
+
+    # Get an object hierarchy modelling the actual queried fields and the input variables for each field
+    # mayeb use graphql-core implementation if it is decent
+    # this embeds the final vars in the hierarchy. that would be most useful, let's assume so...
+    query_sturcture = await get_queried_field_hierarchy(query_string)
+
+    # construct a response dictionary based on walking the query structure recursively
+    response: dict = await get_schema_fields(
+        query_sturcture, schema_from_some_fastapi_dependency
+    )
+
+    return response
+
+
+async def get_schema_fields(query, model: type[BaseModel]) -> dict[str, Any]:
+    # TODO not sure this is any different from get_response
+
+    # Important design criteria here: this is the visitor pattern, so  the tree walking logic is entirely contained in this code, and is not visible to the schema models (don't pollute models) or the query sturcture
+
+    # NB: here we have query and schema, but not response object. so get a response
+
+    # NB: in future Want to cahce identical response objects and re-export them. not sure this really makes sense though
+
+    result = {}
+    async_fields = {}
+
+    # TODO want to wrap this loop up in a pooled await to minimise wlal-clock time
+    # TODO sensible exception handling and None-insertion
+
+    for field in query:
+        # TODO do we wanna use the pydantic export stuff? Not sure it makes sense, makes it a bit more inconsistent with the experience for function resolvers
+        model_field = getattr(model, field.name)
+        if inspect.isawaitable(model_field):
+            # a bit of a fudge to look for awaitables, a field might do this. but it prob works out in the wash
+            #   mebbe look for coro instead, makes more sense
+            # Also implies that we don't support non-async. thta's a mess anyway (using a threadpool safely; propagating context into another thread)
+
+            # TODO this needs to be wrapped in a task
+            async_fields[field.name] = get_value_for_resolver_function(
+                field, model_field
+            )
+        else:
+            result[field.name] = model_field
+
+    # TODO try anyio
+    if async_fields:
+        await batch_await(async_fields.values())
+        for fieldname, task in async_fields.items():
+            # TODO check for NOne where not allowed, and cascade up
+            # TODO do we handle exceptions here? I think not, get_value_for_resolver_function does that. an exception here means that tihs entire object gets invalidated
+            #   if multiple exceptions, which one gets cascaded?
+            #       mebbe choose one arbitrarily and log the rest?
+            #       what if other exceptions *would* occur except execution got short-circuited?
+            #       -> does it really matter?? What are we expecting the client to do if there are multiple reasons why an object couldnt be resolved? Mebbe more info is nice, coudl do in future but meh
+            result[fieldname] = await task  # raises exception, or returns valid value
+
+    return result
+
+
+async def get_value_for_resolver_function(field, coro):
+    # Get a valid value for this field, or raise an exception
+
+    # TODO this func name is too long
+
+    # TODO i would expect to see recursion somewhere here
+    #   i think the problem is that we start with a bASEmodel class, but after the first level we get basemodel instances
+    FIXME
+
+    params: dict[str, Any] = field.params
+    # Looks in some hidden global thingummy, doesn;t try to mash in a dependency thatr conflicts with a param. prob better off using typed values for func params like fastapi
+    dependencies: dict[str, Any] = find_dependencies_for_resolver(
+        coro, excluded=params.keys()
+    )
+
+    params.update(dependencies)
+
+    try:
+        model_field = await coro(**params)
+    except BaseException as ex:
+        # Have to deal with BaseException, mutter mutter
+        # TODO hook into customised exception handlers, possibly customised on the current model (or a parent model in the hierarchy)
+        # TODO if not handled by custom logic and we can convert thsi field into a None, do so
+        # TODO else re-raise
+        raise
+
+    # TODO resolver funcitons don't ge tpydantics strict value checking, so we need tocheck for NOne where not allowed, and cascade up (raise an exception?)
