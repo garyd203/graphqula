7cab7dbb7f3ec64f019c5dcee30c28f1975b7a0e
Gary Donovan <gary@dataro.io>
2026-06-10 23:02:22 +1000

    Spell out purpose of bindings directory

---
+++ src/purql/bindings/__init__.py
@@ -0,0 +1,7 @@
+"""Out-of-the-box bindings for various HTTP server frameworks.
+
+Other bindings are possible, these are provided for convenience.
+
+Note that each binding needs to be in an indpendent module that is not imported by another module,
+so that we don't introduce unnecessary third party dependencies.
+"""
