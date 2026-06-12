028bb58717e11457e4d12ec786e34970c9be551e
Gary Donovan <gary@dataro.io>
2026-06-13 00:07:45 +1000

    #10: WIP for faastAPI binding tests

--- tests/bindings/test_fastapi.py
+++ tests/bindings/test_fastapi.py
@@ -2,35 +2,62 @@ from unittest.mock import AsyncMock, patch
 
 import pytest
 from fastapi import FastAPI
-from fastapi.status import (
+from starlette.status import (
     HTTP_200_OK,
     HTTP_405_METHOD_NOT_ALLOWED,
     HTTP_422_UNPROCESSABLE_ENTITY,
 )
 from fastapi.testclient import TestClient
-from pydantic import BaseModel
 
-from purql import make_schema
+from purql import Schema
 from purql.bindings.fastapi import GraphQLRouter
-from purql.execution import ExecutionError, ExecutionResult
+from purql.core import ExecutionError, ExecutionResult
 
+# TODO check through this file, not convinced about it
+#   patch on module pbject
+#   do we _really_ need to patch
+#   at least 1 proper integration test
 
-class Query(BaseModel):
-    pass
+# TODO redo this setup to just use a real schema with real resovlers - no need for patching, but we may want to add some spies
+
+# TODO do we need to try hypothesis or parametrisation ehre to it4erate through variations of valid input params?
+
+# TODO mroe test case:
+#   trailing `/` sigh
+#   request type
+#   response type
+#   request accept type
+#   actually accept GET
+#   0->many nested known exceptions vs. unhandled top-levle exception
+#   variations of bad input data
+
 
 
 @pytest.fixture()
-def test_client() -> TestClient:
+def schema() -> Schema:
+    schema = Schema()
+
+    @schema.register_query()
+    async def hero() -> str:
+        return "R2-D2"
+
+    @schema.register_mutation()
+    async def update_hero() -> str:
+        return "R2-D2"
+
+    return schema
+
+
+@pytest.fixture()
+def test_client(schema) -> TestClient:
     app = FastAPI()
-    app.include_router(GraphQLRouter(make_schema(query=Query)))
+    app.include_router(GraphQLRouter(schema))
     yield TestClient(app)
 
 
-def test_forwards_parsed_request_to_execute(test_client):
+def test_post_request_should_return_result(schema, test_client):
     # Setup
-    with patch(
-        "purql.bindings.fastapi.execute", new_callable=AsyncMock
-    ) as mock_execute:
+    with patch.object(schema, "execute", new_callable=AsyncMock) as mock_execute:
         mock_execute.return_value = ExecutionResult(data={"hero": "R2-D2"})
 
         # Exercise
@@ -49,17 +76,15 @@ def test_forwards_parsed_request_to_execute(test_client):
 
     mock_execute.assert_awaited_once()
     kwargs = mock_execute.await_args.kwargs
-    assert kwargs["query"] == "{ hero }"
+    assert kwargs["document"] == "{ hero }"
     assert kwargs["operation_name"] == "GetHero"
     assert kwargs["variables"] == {"episode": "EMPIRE"}
 
 
-def test_unhandled_errors_should_return_a_graphql_error(test_client):
+def test_post_request_should_return_internal_graphql_errors(schema, test_client):
     # Setup
-    with patch(
-        # TODO patch on module, not name
-        "purql.bindings.fastapi.execute", new_callable=AsyncMock
-    ) as mock_execute:
+    with patch.object(schema, "execute", new_callable=AsyncMock) as mock_execute:
+        # TODO this is incorrect - the funciton interface needs to be improved
         mock_execute.return_value = ExecutionResult(
             errors=[ExecutionError(message="boom")]
         )
