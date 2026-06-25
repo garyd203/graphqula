c265030e0c2710d9844502e5b7083baf0a2f9898
GaryD <gazza@gazza.id.au>
2026-06-26 08:30:44 +1000

    #21: Outline an implementation for execute()

--- src/graphqula/core.py
+++ src/graphqula/core.py
@@ -141,7 +141,7 @@ class Schema:
         error_handler: BaseErrorHandler | None = None,
         operation_name: str | None = None,
         variables: dict[str, Any] | None = None,
-    ) -> dict[str, Any] | None:
+    ) -> JSONDict:
         """Execute a GraphQL operation against this schema, returning just the result data.
 
         Any errors during field evaluation will be reported to `error_handler`.
@@ -169,9 +169,27 @@ class Schema:
         LOGGER.debug("Handling errors with %s", error_handler)
         error_handler.bind_to_request()
 
-        # TODO note that unhandled exceptions may be propagated out of this function
-        # TODO actually execute the query :-)
-        return None  # FIXME
+        # Parse and validate the document
+        # FIXME not async - use threadpool and async to_thread or equivalent
+        # TODO handle errors
+        # TODO set max_tokens for safety
+        query_ast = parse_graphql(document)
+
+        # FIXME not async
+        # TODO handle errors
+        # TODO set max_errors
+        # TODO do we need to set rules
+        # TODO do we need to set type_info
+        assert self._ast is not None, "Created when the schema was frozen"
+        _validation_errors = validate_graphql(self._ast, query_ast)
+
+        operation = await get_operation(query_ast, operation_name, variables or {})
+
+        # Generate response data for the selected operation
+        # TODO choose self._queries or self._mutations based on op kind
+        assert self._queries is not None, "Created when the schema was frozen"
+        response = await evaluate_operation(operation, self._queries, error_handler)
+        return response
 
     async def _build_ast(self) -> GraphQLSchema:
         """Build an AST representation of the root fields in this schema."""
