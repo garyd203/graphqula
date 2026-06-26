85b4b5bff40309c12e7097ef053cc92283824bfb
GaryD <gazza@gazza.id.au>
2026-06-26 22:15:38 +1000

    #36: Helper to convert graphql-core document AST to a real Operation, currently with fake implementation

---
+++ src/graphqula/ast_helpers.py
@@ -0,0 +1,27 @@
+"""Tools for working with the Schema and Document AST's from `graphql-core` library."""
+
+from typing import Any, Optional
+
+from graphql import DocumentNode
+
+from .document import Operation, OperationKind, FieldGroup, ResponseField
+
+
+# TODO gte a standard union type for var values mebbe. is it worth it?
+# TODO rename mebbe?
+async def get_operation(
+    query_ast: DocumentNode, operation_name: Optional[str], variables: dict[str, Any]
+) -> Operation:
+    """Extract the named operation from this document, converting it to a cleaner data structure."""
+    # FIXME just build a fake operation for now
+    return Operation(
+        kind=OperationKind.QUERY,
+        name=operation_name,
+        children=FieldGroup(
+            fields=[
+                ResponseField(
+                    name_in_schema="hero", name_in_response="hero", arguments=dict()
+                )
+            ]
+        ),
+    )
