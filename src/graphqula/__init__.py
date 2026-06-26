2d02927b6d248004f8c2af14f63fc867cf3c55b9
GaryD <gazza@gazza.id.au>
2026-06-26 21:25:32 +1000

    #21: Configure black ("requires no configuration", they said :-)

--- src/graphqula/__init__.py
+++ src/graphqula/__init__.py
@@ -1,15 +1,7 @@
 # Dedicated re-exports for the public parts of the graphqula interface
 
-from .core import (
-    Schema,
-)
-from .error_handler import (
-    ErrorHandler,
-)
+from .core import Schema
+from .error_handler import ErrorHandler
 from .exceptions import CancelledExecutionError
 
-__all__ = [
-    "CancelledExecutionError",
-    "ErrorHandler",
-    "Schema",
-]
+__all__ = ["CancelledExecutionError", "ErrorHandler", "Schema"]
