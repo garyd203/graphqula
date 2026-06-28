50c1a9500b19f614e82ee7ccb1923fee911e53cb
GaryD <gazza@gazza.id.au>
2026-06-29 09:07:51 +1000

    #36: Shared generic types for internal use

---
+++ src/graphqula/types.py
@@ -0,0 +1,22 @@
+"""General-purpose shared types."""
+
+from enum import Enum
+
+from typing import TypeAlias, Union
+
+#: Type of any raw non-structured value that is passed over-the-wire in a GraphQL
+#: document, variable or response. This is any of the built-in scalars + any enum.
+#: Note that this represents data that has not been converted to any custom scalar
+#: type.
+RawLeafType: TypeAlias = Union[bool | float | int | str, Enum]
+
+#: Primitive types that can appear as a JSON value.
+JSONPrimitive: TypeAlias = bool | float | int | str
+
+#: Structured types that can appear as a JSON value.
+JSONValue: TypeAlias = JSONPrimitive | None | list["JSONValue"] | dict[str, "JSONValue"]
+
+#: A top-level JSON object.
+#: We intentionally don't include None (this should be a specific type declaration if
+#: it is intended), or lists (since we don't use/support top-level JSON lists).
+JSONDict: TypeAlias = dict[str, JSONValue]
