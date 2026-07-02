2245598eaab60334f95eacafe90233d113158e2d
GaryD <gazza@gazza.id.au>
2026-07-02 21:06:47 +1000

    #46: Implement internal types to model a schema, independent of the developer-facing interface

---
+++ src/graphqula/_structure/__init__.py
@@ -0,0 +1,1 @@
+"""Internal types to represent GraphQL structures."""
---
+++ src/graphqula/_structure/schema.py
@@ -0,0 +1,27 @@
+"""Classes to represent a GraphQL Schema.
+
+The module is deliberately decoupled from `graphql-core`, and is intended to provide an
+independent representation of our internal objects that are used to implement a schema
+and use it for executing requests.
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass(frozen=True)
+class DeferredFieldData:
+    """Schema metadata for a deferred field, that needs to execute a custom function in order to get the result."""
+
+    #: Function used to evaluate the value for the field
+    evaluator: DeferredField
+
+    #: Type of the value returned
+    # TODO determine if this is a python type, or a schema type -> schema type i think
+    # TODO can probably do a better job of the Any type
+    result_type: Any
+
+    # TODO dependencies
+    # TODO params
