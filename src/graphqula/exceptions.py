3729c916f3870909138c6d7ba126f56f460a09a4
GaryD <gazza@gazza.id.au>
2026-07-02 22:29:45 +1000

    #21: Move helpers out of the `core` module
    * `DeferredField` -> `types`
    * `SchemaFrozenError` -> `exceptions`

--- src/graphqula/exceptions.py
+++ src/graphqula/exceptions.py
@@ -3,3 +3,7 @@ from __future__ import annotations
 
 class CancelledExecutionError(Exception):
     """Generic error raised when GraphQL execution is cancelled before all response data was determined."""
+
+
+class SchemaFrozenError(Exception):
+    """Raised when we try to modify a frozen schema."""
